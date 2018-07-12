eb7fe562b9e6b9e526ee92e715683fd74994088d
epeuva <epeuva@gmail.com>
2018-07-12 13:30:16 +0200

    Added viewDidLoad code

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -11,11 +11,27 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *headline;
 @property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 
 @end
 
 @implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+    [title setAttributes:@{ NSStrokeWidthAttributeName : @3,
+                            NSStrokeColorAttributeName : self.outlineButton.tintColor }
+                   range: NSMakeRange(0, [title length])
+     ];
+    
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+
+}
+
+
 - (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
     
     [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:(sender.backgroundColor) range:self.body.selectedRange];
@@ -33,10 +49,7 @@
 }
 
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+
 
 
 
