6d2ae8dcf7081cc146f243c48e0d74117756a36e
epeuva <epeuva@gmail.com>
2018-07-13 14:10:54 +0200

    Added empty TextStatsViewController

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by Josep on 13/07/2018.
+//  Copyright © 2018 Epeuva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
---
+++ Attributor/TextStatsViewController.m
@@ -0,0 +1,19 @@
+//
+//  TextStatsViewController.m
+//  Attributor
+//
+//  Created by Josep on 13/07/2018.
+//  Copyright © 2018 Epeuva. All rights reserved.
+//
+
+#import "TextStatsViewController.h"
+
+@interface TextStatsViewController ()
+
+@end
+
+@implementation TextStatsViewController
+
+
+
+@end
